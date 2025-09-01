ccb9f0b4d414c26607ee9bc80661c02974bb17e9
limcaroline <carolineloislim@gmail.com>
2025-09-01 11:15:00 +0800

    Added base and index html files

--- worldawaits/settings.py
+++ worldawaits/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-uz6_&r9mqqubd823inh+_r%*#g=3&p=ittk(cdkbzq)1l$%2o_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -64,7 +65,7 @@ ROOT_URLCONF = "worldawaits.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
